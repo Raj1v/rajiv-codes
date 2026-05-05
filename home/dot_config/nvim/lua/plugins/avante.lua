c261fab4d93a0824e3d7d2f51de04123d10bc0d3
Frederik Rajiv Manichand <rajivmanichand@gmail.com>
2026-05-05 15:45:29 +0000

    nvim: bump lazy-lock and disable avante

--- home/dot_config/nvim/lua/plugins/avante.lua
+++ home/dot_config/nvim/lua/plugins/avante.lua
@@ -1,4 +1,42 @@
 return {
   "yetone/avante.nvim",
-  enabled = false,
+  event = "VeryLazy",
+  version = false,
+  build = "make",
+  opts = {
+    provider = "copilot",
+    auto_suggestions_provider = "copilot",
+    providers = {
+      copilot = {
+        model = "claude-opus-4.7",
+        extra_request_body = {
+          temperature = 0,
+          max_tokens = 8192,
+        },
+      },
+    },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "zbirenbaum/copilot.lua",
+    "stevearc/dressing.nvim",
+    "nvim-tree/nvim-web-devicons",
+    {
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = { insert_mode = true },
+        },
+      },
+    },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = { file_types = { "markdown", "Avante" } },
+      ft = { "markdown", "Avante" },
+    },
+  },
 }
