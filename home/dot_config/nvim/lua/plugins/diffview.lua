e4b5c6ba90abd7d9660afb94f0442b9ac42f5dd4
Frederik Rajiv Manichand <rajivmanichand@gmail.com>
2026-05-05 15:55:07 +0000

    nvim(diffview): use <leader>gv/V/H to avoid LazyVim collisions

--- home/dot_config/nvim/lua/plugins/diffview.lua
+++ home/dot_config/nvim/lua/plugins/diffview.lua
@@ -4,9 +4,9 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory", "DiffviewToggleFiles" },
     keys = {
-      { "<leader>gd", "<cmd>DiffviewOpen origin/main...HEAD<cr>", desc = "Diff vs main (PR view)" },
-      { "<leader>gD", "<cmd>DiffviewClose<cr>", desc = "Close diffview" },
-      { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "File history (current file)" },
+      { "<leader>gv", "<cmd>DiffviewOpen origin/main...HEAD<cr>", desc = "Diff vs main (PR view)" },
+      { "<leader>gV", "<cmd>DiffviewClose<cr>", desc = "Close diffview" },
+      { "<leader>gH", "<cmd>DiffviewFileHistory %<cr>", desc = "File history (current file)" },
     },
   },
 }
